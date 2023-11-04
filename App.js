fbe80fde7b90b02251596e37bb2085b962899a25
Nicolas Claudino <nicolasclaudinodeaguiar@gmail.com>
2023-11-04 15:33:53 -0300

    Programando sensores de luz

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StyleSheet, Text, View, StatusBar, ScrollView, Image } from 'react-native';
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { LightSensor } from "expo-sensors";
 
 export default function App() {
+
+  const [{ illuminance }, definirIluminacao ] = useState({ illuminance: 0})
+
+  useEffect(function() {
+    LightSensor.addListener(definirIluminacao)
+  }, [])
+
+  function VerificarLuz(escuro, claro) {
+    return illuminance > 20 ? claro : escuro
+  }
+
   return (
-    <ScrollView>
-      <StatusBar barStyle={"dark-content"} backgroundColor={"#FFFBEB"}/>
+    <ScrollView
+      style={{ backgroundColor: VerificarLuz("#FFFBEB", "#20262E") }}
+    >
+      <StatusBar barStyle={VerificarLuz("dark-content", "light-content")} 
+      backgroundColor={VerificarLuz("#FFFBEB", "#20262E")}/>
+
+      <View>
+        <Text
+          style={{ color: VerificarLuz("#222", "#fff"), fontSize: 32, textAlign: "center"}}
+        > Sensores do Smartphone!</Text>
+        <Text
+          style={{ color: VerificarLuz("#222", "#fff"), fontSize: 18, textAlign: "left"}}
+        > Testando a iluminação do ambiente</Text>
+        <Text
+          style={{ color: VerificarLuz("#222", "#fff"), fontSize: 10, textAlign: "left"}}
+        > { illuminance }</Text>
+
+        <Image source={{ uri: "http://picsum.photos/200/200" }} style={{ height: 200}}/>
+      </View>
     </ScrollView>
   );
 }
